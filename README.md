db19aede2931db6725943489a652bd9cf3fbe651
Paolo <s319440@studenti.polito.it>
2024-04-23 16:26:32 +0200

    feat: load config from file

--- README.md
+++ README.md
@@ -10,6 +10,27 @@ Real time **Serial CSV** to **UDP JSON** stream translator
 pip install -r requirements.txt
 ```
 
+### CONFIG file
+To configure the parameters of the program you have to create a 'CONFIG.txt' file with the following parameters:
+```txt
+VALUES=PARAM1, PARAM2, NULL, PARAM3
+NEWLINE=\n
+SEPARATOR=,
+BAUDRATE=9600
+UDP_PORT=5000
+SERIAL_PORT=/dev/ttyUSB0
+```
+The 'VALUES' params indicate the order of the values in the CSV string. The 'NULL' value indicates that the value is not going to be sent. The 'NEWLINE' param indicates the character that separates the CSV strings. The 'SEPARATOR' param indicates the character that separates the values in the CSV string. The 'BAUDRATE' param indicates the baudrate of the serial port. The 'UDP_PORT' param indicates the port of the UDP server. The 'SERIAL_PORT' param indicates the serial port to use.
+
+With this configuration you can send the following JSON (assuming CSV data is: 1,2,3,4\n):
+```json
+{
+    "PARAM1": 1,
+    "PARAM2": 2,
+    "PARAM3": 4
+}
+```
+
 ...
 
 ### Build executable
--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from backend.functions import start_connection_controller
 import threading
 
 class GUI:
-    PATH_JSON_MODEL = "Path"
+    # Config parameters
+    PATH_CONFIG_MODEL = "Path"
+    VALUES="example"
+    NEWLINE=b'\n'
+    SEPARATOR=b','
+    BAUDRATE=9600
+    UDP_PORT=5000
+    SERIAL_PORT=b'/dev/ttyUSB0'
+
+    # Flag
     connected = False
 
     def __init__(self):
@@ -30,28 +39,28 @@ class GUI:
         self.label = tk.Label(self.app, text="Press connect to start converting Serial messages \nto JSON and stream them to UDP (localhost).")
         self.label.pack(pady=10)
 
+        # Path to CONFIG
+        self.label_path_config_model = tk.Label(self.app, text="Select path to CONFIG model to use:")
+        self.label_path_config_model.pack()
+        self.textbox_path_config_model = tk.Text(self.app, height=1, width=50)
+        self.textbox_path_config_model.pack()
+        self.textbox_path_config_model.insert("1.0", self.PATH_CONFIG_MODEL)
+        self.browse_button_config_model = tk.Button(self.app, text="Browse", command=self.browse_file_config_model)
+        self.browse_button_config_model.pack()
+
         # Local UDP Port
         self.label_udp_port = tk.Label(self.app, text="Local UDP port:")
         self.label_udp_port.pack()
         self.textbox_udp_port = tk.Text(self.app, height=1, width=30)
-        self.textbox_udp_port.insert("1.0", "1000")                        # Default UDP port
+        self.textbox_udp_port.insert("1.0", self.UDP_PORT)                        
         self.textbox_udp_port.pack()
 
         # Serial Port
         self.label_serial_port = tk.Label(self.app, text="Serial port:")
         self.label_serial_port.pack()
         self.textbox_serial_port = tk.Text(self.app, height=1, width=30)
-        self.textbox_serial_port.insert("1.0", "COM3")                        # Default Serial port
+        self.textbox_serial_port.insert("1.0", self.SERIAL_PORT)                       
         self.textbox_serial_port.pack()
-
-        # Path JSON model
-        self.label_path_json_model = tk.Label(self.app, text="Select path to JSON data model to use:")
-        self.label_path_json_model.pack()
-        self.textbox_path_json_model = tk.Text(self.app, height=1, width=50)
-        self.textbox_path_json_model.pack()
-        self.textbox_path_json_model.insert("1.0", self.PATH_JSON_MODEL)
-        self.browse_button_json_model = tk.Button(self.app, text="Browse", command=self.browse_file_json_model)
-        self.browse_button_json_model.pack()
 
         # Connect button
         self.connect_button = tk.Button(self.app, text="Connect", command=self.connect_thread, font=("Arial", 14, "bold"), width=15, height=2)
@@ -65,13 +74,14 @@ class GUI:
         self.app.mainloop()
 
     # ---------- FUNCTIONS ---------------------------------------------
-    def browse_file_json_model(self):
-        self.file_path_json_model = filedialog.askopenfilename()
-        self.textbox_path_json_model.delete("1.0", tk.END)
-        self.textbox_path_json_model.insert("1.0", self.file_path_json_model)
+    def browse_file_config_model(self):
+        global PATH_CONFIG_MODEL
 
-        global PATH_JSON_MODEL
-        self.PATH_JSON_MODEL = self.file_path_json_model
+        self.PATH_CONFIG_MODEL = filedialog.askopenfilename()
+        self.textbox_path_config_model.delete("1.0", tk.END)
+        self.textbox_path_config_model.insert("1.0", self.PATH_CONFIG_MODEL)
+
+        self.startup()
 
     def connect_thread(self):
         # Start a new thread for the connect function
@@ -79,11 +89,44 @@ class GUI:
 
     def connect(self):
         # Get the port from the textbox
-        udp_port = self.textbox_udp_port.get("1.0", "end-1c")
-        serial_port_name = self.textbox_serial_port.get("1.0", "end-1c")
+        UDP_PORT = self.textbox_udp_port.get("1.0", "end-1c")
+        SERIAL_PORT = self.textbox_serial_port.get("1.0", "end-1c")
 
-        start_connection_controller(udp_port, serial_port_name, self.PATH_JSON_MODEL)
+        start_connection_controller(UDP_PORT, SERIAL_PORT, self.VALUES)  # TO CHANGE PATH_CONFIG_MODEL functions.py
 
         self.label_connected.pack()
+    
+    def startup(self):
+        config = {
+            'VALUES': '',
+            'NEWLINE': '',
+            'SEPARATOR': '',
+            'BAUDRATE': '',
+            'UDP_PORT': '',
+            'SERIAL_PORT': ''
+        }
+        with open(self.PATH_CONFIG_MODEL, 'r') as f:
+            lines = f.readlines()
+            if lines:
+                for line in lines:
+                    for key in config.keys():
+                        if line.startswith(f'{key}='):
+                            config[key] = line.split('=')[1].strip()
+        self.VALUES = config['VALUES']
+        self.NEWLINE = config['NEWLINE']
+        self.SEPARATOR = config['SEPARATOR']
+        self.BAUDRATE = config['BAUDRATE']
+        self.UDP_PORT = config['UDP_PORT']
+        self.SERIAL_PORT = config['SERIAL_PORT']
+
+        # Update the interface with new values
+        self.update_interface()
+
+    def update_interface(self):
+        self.textbox_udp_port.delete("1.0", tk.END)
+        self.textbox_udp_port.insert("1.0", str(self.UDP_PORT))
+        self.textbox_serial_port.delete("1.0", tk.END)
+        self.textbox_serial_port.insert("1.0", str(self.SERIAL_PORT))
+
 
 GUI()
